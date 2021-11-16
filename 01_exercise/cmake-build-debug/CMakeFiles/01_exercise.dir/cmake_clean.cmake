2b9d2a9970f83ec49437af02c21f5483b2ee5263
jrmejiaa <jairo.mejia.2708@gmail.com>
2021-11-16 17:50:02 +0100

    add CLion Project

---
+++ 01_exercise/cmake-build-debug/CMakeFiles/01_exercise.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "01_exercise"
+  "01_exercise.pdb"
+  "CMakeFiles/01_exercise.dir/01/stack-threadsafe.cpp.o"
+  "CMakeFiles/01_exercise.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_exercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
